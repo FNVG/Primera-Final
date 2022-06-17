b076803ffe3b71dec2d17589a5eb9811df166cda
FNVG <87998232+FNVG@users.noreply.github.com>
2022-06-17 15:58:13 -0400

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -12,67 +12,132 @@
 #include <time.h>
 #include<sys/wait.h>
 #include <termios.h>
-#include <math.h>
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t condprint = PTHREAD_COND_INITIALIZER;
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>
+#define MAX 20
+int vector[MAX];
+int numeroBuscado = -1;
+int posicion = -1;
+int numeroHilos = 0;
+pthread_mutex_t mutexA = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexB = PTHREAD_MUTEX_INITIALIZER;
+bool encontrado = false;
+sem_t semaphore = {0};
+pthread_t IDproceso;
+int id;
 
 struct Dato {
-  int id;
-  int number;
+ int id;
+  int comienzo;
+  int final;
 };
 
 
 
-int next_thread = 0;
-int mythread=0;
 
-void* thread_function(void *argcthread) {
-  pthread_mutex_lock (&mutex);        
+void *threat_function(void *argcthread) {
+pthread_mutex_lock(&mutexB);
 struct Dato argumento = *(struct Dato*) argcthread;  
-  
-  while (next_thread != argumento.id) 
-      pthread_cond_wait(&condprint, &mutex);
+ int i = argumento.comienzo;
 
-  unsigned long long factorial = 1;
-  for(int x=1;x<=argumento.number;x++)
-       factorial=factorial*x;
-  
-  printf("Factorial de %d es %llu\n",argumento.number, factorial);
-next_thread++;
-pthread_cond_broadcast(&condprint);
-pthread_mutex_unlock (&mutex);
-  return NULL;
+  while ((i <= argumento.final) && (!encontrado)) {  
+    if (vector[i] == numeroBuscado) {
+     posicion = i;
+     printf("\n[Encontrado]\n");
+      id = argumento.id;
+    IDproceso = pthread_self();
+    encontrado = true;
+      }
+   i++;
+}
+  pthread_mutex_unlock(&mutexB);
+   return NULL;
   }
-
-
-  
 
 
 
 int main(int argc, char *argv[]) {
-   
-    pthread_t thread[argc-1];
-    struct Dato arr_datos[argc-1];
-    int k=0;
+  if ((argc -1) != 2) {
+    printf("ERROR: La cantidad de datos ingresada no es valida ");
+    exit(EXIT_FAILURE);
+  }
   
-    for(int i=1;i<argc;i++) {
-          arr_datos[k].id = k;
-          arr_datos[k].number = atoi(argv[i]);  
-           
-   
-      pthread_create(&thread[k], NULL, thread_function,&arr_datos[k]);
+  int lower = 0, upper = 9, count = 1;
+pthread_t tid_signal;
+   int VALUE = 0;
 
-       k++;
-      
-      }
-  
-  
-   for(int i=0;i<argc-1;i++) {
-        pthread_join(thread[i], NULL);
+  printf("\n--------Vector---------\n");
+  srand(0);
+    for (int i =0 ; i< MAX; i++) {
+          vector [i] =  (rand() %
+           (upper - lower + 1)) + lower;
+        printf("Posicion %d -> %d\n", i,  vector[i]);
+    }
+printf("\n\n");
+  numeroBuscado = atoi(argv[1]);
+  numeroHilos = atoi(argv[2]);
+     
+   int c = MAX / numeroHilos;
+  int r = MAX % numeroHilos;
+  pthread_t tids[numeroHilos];
+int recorrido_datos [numeroHilos];
+  for (int i=0; i<numeroHilos; i++) {
+    if (r > 0) {
+      recorrido_datos[i] = c+1;
+      r--;
+    } else {
+      recorrido_datos[i] = c;
+    }
+  }
+   struct Dato arr_datos[numeroHilos];
+
+ int comienzo =0;
+    for (int i=0; i<numeroHilos; i++) {
+      arr_datos[i].id = i;
+      arr_datos[i].comienzo = comienzo;
+      arr_datos[i].final = recorrido_datos[i] +comienzo -1;
+      comienzo = recorrido_datos[i] +comienzo;
+    } 
+printf("\n-----Rango de los hilos-------\n");
+  for (int i=0; i<numeroHilos; i++) {
+    printf("Hilo %d rango [%d,%d]\n",arr_datos[i].id, arr_datos[i].comienzo, arr_datos[i].final);
+  }
+
+printf("\n\n");
+  //Hilos que buscan el numero
+  for (int i=0; i<numeroHilos; i++) {
+ if ( pthread_create(&tids[i], NULL,threat_function, &arr_datos[i])) {
+    printf("\nERROR: pthread_create de los hilos\n");
+     exit(EXIT_FAILURE);
+ }
 
     
-    }
+  }
 
+  
+  
+ //Join de los hilos
+  for (int i=0; i<numeroHilos; i++) {
+  if (pthread_join(tids[i], NULL) !=0 ){
+    printf("ERROR: funcion pthread_join de hilos que buscan el numero");
+    exit(EXIT_FAILURE);
+  }  
+  }
+  
+  printf("\nHilos terminados......\n\n\n");
+  
+  if(posicion == -1)
+  {
+    printf("\n[No encontrado]\n");
+  } else {
+    printf("\nEl hilo %d encontro el numero\n", id);
+    printf("\nID Thread: %lu\n", IDproceso);
+    printf("\nPosicion del elemento en el vector : %d\n", posicion);
+  }
+    
+
+  
+  //Solucionar el hilo que termina los demas hilos despues
   
   return 0;
 }
